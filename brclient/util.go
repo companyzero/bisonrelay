d3a16bb46789bd7d2509ab50665238649eb3f358
miki <mikitotefu@companyzero.com>
2022-02-01 13:31:10 +0000

    brclient: extract parseEmbedArgs
    Part of refactoring.

--- brclient/util.go
+++ brclient/util.go
@@ -212,54 +212,61 @@ func (args embeddedArgs) String() string {
 
 var embedRegexp = regexp.MustCompile(`--embed\[.*?\]--`)
 
+func parseEmbedArgs(rawEmbedStr string) embeddedArgs {
+	// Copy everything between the [] (the raw argument list).
+	start, end := strings.Index(rawEmbedStr, "["), strings.LastIndex(rawEmbedStr, "]")
+	rawArgs := rawEmbedStr[start+1 : end]
+
+	// Split args by comma.
+	splitArgs := strings.Split(rawArgs, ",")
+
+	// Decode args.
+	var args embeddedArgs
+	for _, a := range splitArgs {
+		// Split by "="
+		kv := strings.SplitN(a, "=", 2)
+		if len(kv) != 2 {
+			continue
+		}
+		k, v := kv[0], kv[1]
+		switch k {
+		case "name":
+			args.name = v
+		case "type":
+			args.typ = v
+		case "data":
+			decoded, err := base64.StdEncoding.DecodeString(v)
+			if err != nil {
+				decoded = []byte(fmt.Sprintf("[err decoding data: %v]", err))
+			}
+			args.data = decoded
+		case "alt":
+			decoded, err := url.PathUnescape(v)
+			if err != nil {
+				decoded = fmt.Sprintf("[err processing alt: %v]", err)
+			}
+			args.alt = decoded
+		case "download":
+			// Ignore the error and leave download empty.
+			args.download.FromString(v)
+		case "filename":
+			args.filename = v
+		case "size":
+			args.size, _ = strconv.ParseUint(v, 10, 64)
+		case "cost":
+			args.cost, _ = strconv.ParseUint(v, 10, 64)
+		}
+	}
+
+	return args
+}
+
 // replaceEmbeds replaces all the embeds tags of the given text with the result
 // of the calling function.
 func replaceEmbeds(src string, replF func(args embeddedArgs) string) string {
 	return embedRegexp.ReplaceAllStringFunc(src, func(repl string) string {
-		// Copy everything between the [] (the raw argument list).
-		start, end := strings.Index(repl, "["), strings.LastIndex(repl, "]")
-		rawArgs := repl[start+1 : end]
-
-		// Split args by comma.
-		splitArgs := strings.Split(rawArgs, ",")
-
 		// Decode args.
-		var args embeddedArgs
-		for _, a := range splitArgs {
-			// Split by "="
-			kv := strings.SplitN(a, "=", 2)
-			if len(kv) != 2 {
-				continue
-			}
-			k, v := kv[0], kv[1]
-			switch k {
-			case "name":
-				args.name = v
-			case "type":
-				args.typ = v
-			case "data":
-				decoded, err := base64.StdEncoding.DecodeString(v)
-				if err != nil {
-					decoded = []byte(fmt.Sprintf("[err decoding data: %v]", err))
-				}
-				args.data = decoded
-			case "alt":
-				decoded, err := url.PathUnescape(v)
-				if err != nil {
-					decoded = fmt.Sprintf("[err processing alt: %v]", err)
-				}
-				args.alt = decoded
-			case "download":
-				// Ignore the error and leave download empty.
-				args.download.FromString(v)
-			case "filename":
-				args.filename = v
-			case "size":
-				args.size, _ = strconv.ParseUint(v, 10, 64)
-			case "cost":
-				args.cost, _ = strconv.ParseUint(v, 10, 64)
-			}
-		}
+		args := parseEmbedArgs(repl)
 		return replF(args)
 	})
 }
