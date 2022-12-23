013aa95b0b7483b3e9e3c042037a0bc382a66bbd
miki <mikitotefu@companyzero.com>
2022-12-23 16:18:37 +0000

    bruig: make snackbar errors copyable
    fix #12
    
    needs to add some indicator that text was copied and improve the
    coloring when hovering.

--- bruig/flutterui/bruig/lib/components/snackbars.dart
+++ bruig/flutterui/bruig/lib/components/snackbars.dart
@@ -1,13 +1,16 @@
+import 'package:bruig/components/copyable.dart';
 import 'package:flutter/material.dart';
 
 void showErrorSnackbar(BuildContext context, String msg) {
   ScaffoldMessenger.of(context).showSnackBar(SnackBar(
       backgroundColor: Theme.of(context).errorColor,
-      content: Text(msg, style: const TextStyle(color: Color(0xFFE4E3E6)))));
+      content: Copyable(msg, const TextStyle(color: Color(0xFFE4E3E6)),
+          showSnackbar: false)));
 }
 
 void showSuccessSnackbar(BuildContext context, String msg) {
   ScaffoldMessenger.of(context).showSnackBar(SnackBar(
       backgroundColor: Colors.green[300],
-      content: Text(msg, style: const TextStyle(color: Color(0xFFE4E3E6)))));
+      content: Copyable(msg, const TextStyle(color: Color(0xFFE4E3E6)),
+          showSnackbar: false)));
 }
