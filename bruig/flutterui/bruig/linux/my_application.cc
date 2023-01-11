a74490042921647a497bae8e06ffc6394169c847
Alex Yocom-Piatt <ay-p@decred.org>
2023-01-11 12:15:35 -0600

    Add linux build script for AppImage and tar

--- bruig/flutterui/bruig/linux/my_application.cc
+++ bruig/flutterui/bruig/linux/my_application.cc
@@ -40,12 +40,12 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar *header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "bruig");
+    gtk_header_bar_set_title(header_bar, "Bison Relay");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   }
   else {
-    gtk_window_set_title(window, "bruig");
+    gtk_window_set_title(window, "Bison Relay");
   }
 
   gtk_window_set_default_size(window, 1024, 768);
