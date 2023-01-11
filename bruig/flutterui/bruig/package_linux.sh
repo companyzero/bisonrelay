768ad1802a8f2e6c5f14561375531fc2cc2780b6
Alex Yocom-Piatt <ay-p@decred.org>
2023-01-11 12:15:35 -0600

    Add linux build script for AppImage and tar

---
+++ bruig/flutterui/bruig/package_linux.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+VERSION=v0.1.2
+TAR_NAME=bisonrelay-linux-amd64-$VERSION.tar.gz
+BUILD_DIR=build/linux/x64/release/bundle
+APPIMAGE_DIR=BisonRelay/
+APPRUN=AppRun
+DESKTOP_FILE=BisonRelay.desktop
+ICON_FILE=assets/images/icon.png
+
+flutter clean
+flutter build linux --release
+
+if [ -d "$APPIMAGE_DIR" ]; then
+    printf '%s\n' "Removing Lock ($APPIMAGE_DIR)"
+    rm -rf "$APPIMAGE_DIR"
+fi
+mv $BUILD_DIR $APPIMAGE_DIR
+
+tar -czf $TAR_NAME $APPIMAGE_DIR
+
+cp $APPRUN $APPIMAGE_DIR
+
+chmod +x $APPRUN
+
+cp $DESKTOP_FILE $APPIMAGE_DIR
+
+cp $ICON_FILE $APPIMAGE_DIR/bisonrelay.png
+
+if ! type appimagetool-x86_64.AppImage > /dev/null; then
+    printf '%s\n' "appimagetool-x86_64.AppImage (github.com/AppImage/AppImage/Kit) required to be installed for appimage building"
+    exit 1
+fi
+
+appimagetool-x86_64.AppImage $APPIMAGE_DIR
+
+mv Bison_Relay-x86_64.AppImage BisonRelay-$VERSION.AppImage
