754569031d0cfb8f2b370a6dbbccc16ab421b170
miki <mikitotefu@companyzero.com>
2023-01-19 14:01:21 +0000

    clientdb: store push payment status for unacked RMs
    This adds functions to the clientdb to allow the client to store the
    payment status for unacked RMs. This will be necessary to handle restart
    cases during parallel RM pushing.

--- client/clientdb/rvs.go
+++ client/clientdb/rvs.go
@@ -108,3 +108,80 @@ func (db *DB) MarkRVUnpaid(tx ReadWriteTx, rv ratchet.RVPoint) error {
 	}
 	return err
 }
+
+type paidPush struct {
+	AttemptTime time.Time `json:"attempt_time"`
+	Invoice     string    `json:"invoice"`
+}
+
+// StorePushPaymentAttempt stores that there's an inflight payment attempt to
+// pay for pushing to the specified RV with the passed invoice. time refers
+// to the time the attempt started.
+func (db *DB) StorePushPaymentAttempt(tx ReadWriteTx, rv ratchet.RVPoint, invoice string, time time.Time) error {
+	prv := paidPush{AttemptTime: time, Invoice: invoice}
+	filename := filepath.Join(db.root, paidPushesDir, rv.String())
+	return db.saveJsonFile(filename, &prv)
+}
+
+// HasPushPaymentAttempt returns the data for an attempt to pay to push to the
+// specified RV, if one exists. It returns an empty invoice with a nil error
+// if there is no attempt.
+func (db *DB) HasPushPaymentAttempt(tx ReadTx, rv ratchet.RVPoint) (string, time.Time, error) {
+	filename := filepath.Join(db.root, paidPushesDir, rv.String())
+
+	var prv paidPush
+	err := db.readJsonFile(filename, &prv)
+	if errors.Is(err, ErrNotFound) {
+		return "", time.Time{}, nil
+	}
+	if err != nil {
+		return "", time.Time{}, err
+	}
+
+	return prv.Invoice, prv.AttemptTime, nil
+}
+
+// DeletePushPaymentAttempt removes the attempt to pay to push to the specified
+// RV.
+func (db *DB) DeletePushPaymentAttempt(tx ReadWriteTx, rv ratchet.RVPoint) error {
+	filename := filepath.Join(db.root, paidPushesDir, rv.String())
+	return removeIfExists(filename)
+}
+
+// CleanupPushPaymentAttempts removes all registered attempts to pay to push
+// to RVs if they are older than the passed limit time.
+func (db *DB) CleanupPushPaymentAttempts(tx ReadWriteTx, limit time.Time) error {
+	dir := filepath.Join(db.root, paidPushesDir)
+	files, err := os.ReadDir(dir)
+	if os.IsNotExist(err) {
+		return nil
+	}
+	if err != nil {
+		return err
+	}
+
+	total := 0
+	for _, f := range files {
+		var prv paidPush
+		filename := filepath.Join(dir, f.Name())
+		err := db.readJsonFile(filename, &prv)
+		if err != nil {
+			db.log.Debugf("Unable to read json file %s: %v", filename, err)
+			continue
+		}
+		if prv.AttemptTime.Before(limit) {
+			err = os.Remove(filename)
+			if err != nil {
+				db.log.Debugf("Unable to remove file %s: %v",
+					filename, err)
+				continue
+			}
+			total += 1
+		}
+	}
+	if total > 0 {
+		db.log.Debugf("Cleaned up %d push payment attempts", total)
+	}
+
+	return nil
+}
