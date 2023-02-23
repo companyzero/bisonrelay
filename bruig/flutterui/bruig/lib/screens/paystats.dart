c03025af59b3c0aaf77f4ef63d746b10840ab2d6
Alex Yocom-Piatt <ay-p@decred.org>
2023-02-23 03:43:13 -0500

    bruig: Add paystats sent/received summaries

--- bruig/flutterui/bruig/lib/screens/paystats.dart
+++ bruig/flutterui/bruig/lib/screens/paystats.dart
@@ -6,6 +6,7 @@ import 'package:golib_plugin/definitions.dart';
 import 'package:golib_plugin/golib_plugin.dart';
 import 'package:golib_plugin/util.dart';
 import 'package:tuple/tuple.dart';
+import 'package:bruig/components/empty_widget.dart';
 
 class PayStatsScreenTitle extends StatelessWidget {
   const PayStatsScreenTitle({Key? key}) : super(key: key);
@@ -30,7 +31,10 @@ class _PayStatsScreenState extends State<PayStatsScreen> {
   List<Tuple3<String, String, UserPayStats>> stats = []; // UID,nick,stat
   int selectedIndex = -1;
   List<PayStatsSummary> userStats = [];
-  ScrollController userStatsCtrl = ScrollController();
+  ScrollController userStatsSentCtrl = ScrollController();
+  ScrollController userStatsReceivedCtrl = ScrollController();
+  int userStatsTotalReceived = 0;
+  int userStatsTotalSent = 0;
 
   void listPayStats() async {
     try {
@@ -63,6 +67,15 @@ class _PayStatsScreenState extends State<PayStatsScreen> {
       var newUserStats = await Golib.summarizeUserPayStats(stats[index].item1);
       setState(() {
         userStats = newUserStats;
+        userStatsTotalReceived = 0;
+        userStatsTotalSent = 0;
+        for (int i = 0; i < userStats.length; i++) {
+          if (userStats[i].total > 0) {
+            userStatsTotalReceived += userStats[i].total;
+          } else {
+            userStatsTotalSent += userStats[i].total;
+          }
+        }
       });
     } catch (exception) {
       showErrorSnackbar(context, "Unable to fetch user pay stats: $exception");
@@ -99,7 +112,7 @@ class _PayStatsScreenState extends State<PayStatsScreen> {
       padding: const EdgeInsets.all(16),
       child: Column(children: [
         Expanded(
-          flex: 5,
+          flex: 2,
           child: ListView.builder(
               itemCount: stats.length,
               padding: const EdgeInsets.all(0),
@@ -114,8 +127,11 @@ class _PayStatsScreenState extends State<PayStatsScreen> {
                             : otherBackgroundColor,
                         child: Row(children: [
                           SizedBox(
-                              width: 75,
-                              child: Text(stats[index].item2,
+                              width: 100,
+                              child: Text(
+                                  stats[index].item2.isNotEmpty
+                                      ? stats[index].item2
+                                      : "User fees",
                                   style: const TextStyle(fontSize: 11))),
                           const Text(" Sent ", style: TextStyle(fontSize: 11)),
                           SizedBox(
@@ -151,19 +167,75 @@ class _PayStatsScreenState extends State<PayStatsScreen> {
                   )),
         ),
         const Divider(),
-        Expanded(
-          flex: 2,
-          child: SimpleInfoGrid(
-            userStats
-                .map<Tuple2<Widget, Widget>>((e) => Tuple2(
-                      Text(e.prefix, style: TextStyle(color: textColor)),
-                      Text(formatDCR(milliatomsToDCR(e.total)),
-                          style: TextStyle(color: textColor)),
-                    ))
-                .toList(),
-            controller: userStatsCtrl,
-          ),
-        ),
+        userStats.isNotEmpty
+            ? Expanded(
+                flex: 2,
+                child: Row(children: [
+                  Expanded(
+                    flex: 2,
+                    child: Column(children: [
+                      Row(children: [
+                        Text("Total Sent", style: TextStyle(color: textColor)),
+                        const SizedBox(width: 50),
+                        Text(
+                            textAlign: TextAlign.right,
+                            formatDCR(milliatomsToDCR(userStatsTotalSent)),
+                            style: TextStyle(color: textColor)),
+                      ]),
+                      const Divider(),
+                      Expanded(
+                          child: SimpleInfoGrid(
+                        userStats
+                            .map<Tuple2<Widget, Widget>>((e) => Tuple2(
+                                e.total < 0
+                                    ? Text(e.prefix,
+                                        style: TextStyle(color: textColor))
+                                    : const Empty(),
+                                e.total < 0
+                                    ? Text(formatDCR(milliatomsToDCR(e.total)),
+                                        style: TextStyle(color: textColor))
+                                    : const Empty()))
+                            .toList(),
+                        controller: userStatsSentCtrl,
+                      ))
+                    ]),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Row(children: [
+                            Text("Total Received",
+                                style: TextStyle(color: textColor)),
+                            const SizedBox(width: 50),
+                            Text(
+                                textAlign: TextAlign.right,
+                                formatDCR(
+                                    milliatomsToDCR(userStatsTotalReceived)),
+                                style: TextStyle(color: textColor)),
+                          ]),
+                          const Divider(),
+                          Expanded(
+                              child: SimpleInfoGrid(
+                            userStats
+                                .map<Tuple2<Widget, Widget>>((e) => Tuple2(
+                                    e.total > 0
+                                        ? Text(e.prefix,
+                                            style: TextStyle(color: textColor))
+                                        : const Empty(),
+                                    e.total > 0
+                                        ? Text(
+                                            formatDCR(milliatomsToDCR(e.total)),
+                                            style: TextStyle(color: textColor))
+                                        : const Empty()))
+                                .toList(),
+                            controller: userStatsReceivedCtrl,
+                          ))
+                        ]),
+                  )
+                ]))
+            : const Empty(),
       ]),
     );
   }
