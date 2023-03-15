45094f0d3a973c01914563dad18c100860bcbeac
Alex Yocom-Piatt <ay-p@decred.org>
2023-03-15 11:15:43 -0500

    bruig: Remove decprecated window_size package

--- bruig/flutterui/bruig/windows/flutter/generated_plugin_registrant.cc
+++ bruig/flutterui/bruig/windows/flutter/generated_plugin_registrant.cc
@@ -10,7 +10,6 @@
 #include <screen_retriever/screen_retriever_plugin.h>
 #include <url_launcher_windows/url_launcher_windows.h>
 #include <window_manager/window_manager_plugin.h>
-#include <window_size/window_size_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   GolibPluginCApiRegisterWithRegistrar(
@@ -21,6 +20,4 @@ void RegisterPlugins(flutter::PluginRegistry* registry) {
       registry->GetRegistrarForPlugin("UrlLauncherWindows"));
   WindowManagerPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("WindowManagerPlugin"));
-  WindowSizePluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("WindowSizePlugin"));
 }
