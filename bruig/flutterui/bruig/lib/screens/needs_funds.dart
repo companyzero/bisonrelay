3fc0c7bf587b464f920f1102eec5f21e6c130699
miki <mikitotefu@companyzero.com>
2023-03-31 15:18:05 +0000

    bruig: add invite funds redeeming to onboard

--- bruig/flutterui/bruig/lib/screens/needs_funds.dart
+++ bruig/flutterui/bruig/lib/screens/needs_funds.dart
@@ -2,9 +2,12 @@ import 'dart:async';
 
 import 'package:bruig/components/copyable.dart';
 import 'package:bruig/components/buttons.dart';
+import 'package:bruig/components/empty_widget.dart';
 import 'package:bruig/components/snackbars.dart';
 import 'package:bruig/models/notifications.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:golib_plugin/definitions.dart';
 import 'package:golib_plugin/golib_plugin.dart';
 import 'package:golib_plugin/util.dart';
 import 'package:qr_flutter/qr_flutter.dart';
@@ -23,6 +26,9 @@ class _NeedsFundsScreenState extends State<NeedsFundsScreen> {
   int unconfirmedBalance = 0;
   Timer? updateTimer;
   bool createdNeedsChanNtf = false;
+  bool redeeming = false;
+  RedeemedInviteFunds? redeemed;
+  bool? forwardIfBalance;
 
   void getNewAddress() async {
     try {
@@ -51,11 +57,16 @@ class _NeedsFundsScreenState extends State<NeedsFundsScreen> {
           widget.ntfns.addNtfn(AppNtfn(AppNtfnType.walletNeedsChannels));
           createdNeedsChanNtf = true;
 
-          Navigator.of(context, rootNavigator: true).pop();
-          Navigator.of(context, rootNavigator: true)
-              .pushNamed("/needsOutChannel");
-          done = true;
+          if (forwardIfBalance ?? false) {
+            Navigator.of(context, rootNavigator: true).pop();
+            Navigator.of(context, rootNavigator: true)
+                .pushNamed("/needsOutChannel");
+            done = true;
+          }
+          forwardIfBalance = false;
         }
+      } else {
+        forwardIfBalance = true;
       }
     } catch (exception) {
       showErrorSnackbar(context, "Unable to update wallet balance: $exception");
@@ -63,6 +74,28 @@ class _NeedsFundsScreenState extends State<NeedsFundsScreen> {
       if (!done) {
         updateTimer = Timer(const Duration(seconds: 5), updateBalance);
       }
+    }
+  }
+
+  void redeemFunds() async {
+    try {
+      // Decode the invite and send to the user verification screen.
+      var filePickRes = await FilePicker.platform.pickFiles();
+      if (filePickRes == null) return;
+      var filePath = filePickRes.files.first.path;
+      if (filePath == null) return;
+      filePath = filePath.trim();
+      if (filePath == "") return;
+      var invite = await Golib.decodeInvite(filePath);
+      if (invite.invite.funds == null) {
+        throw "Invite does not include funds";
+      }
+      setState(() => redeeming = true);
+      var res = await Golib.redeemInviteFunds(invite.invite.funds!);
+      setState(() => redeemed = res);
+    } catch (exception) {
+      setState(() => redeeming = false);
+      showErrorSnackbar(context, "Unable to redeem invite funds: $exception");
     }
   }
 
@@ -77,6 +110,36 @@ class _NeedsFundsScreenState extends State<NeedsFundsScreen> {
   void dispose() {
     updateTimer?.cancel();
     super.dispose();
+  }
+
+  Widget buildFundsWidget(BuildContext context) {
+    var secondaryTextColor = const Color(0xFFE4E3E6);
+    var ts = TextStyle(
+        color: secondaryTextColor, fontSize: 13, fontWeight: FontWeight.w300);
+
+    if (redeemed != null) {
+      var total = formatDCR(atomsToDCR(redeemed!.total));
+      return Column(children: [
+        Text("Redeemed $total on the following tx:", style: ts),
+        Copyable(redeemed!.txid, ts),
+        Text("The funds will be available after the tx is mined.", style: ts),
+      ]);
+    }
+
+    if (redeeming) {
+      return Column(children: [
+        Text("Attempting to redeem funds from invite file.\n", style: ts),
+      ]);
+    }
+
+    return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+      Text('''
+If someone sent you an invite file with funds, you  may also attempt
+to redeem it by clicking in the button.
+''', style: ts),
+      const SizedBox(width: 18),
+      ElevatedButton(onPressed: redeemFunds, child: const Text("Redeem Funds")),
+    ]);
   }
 
   @override
@@ -114,7 +177,7 @@ class _NeedsFundsScreenState extends State<NeedsFundsScreen> {
                   padding: const EdgeInsets.all(10),
                   child: Column(
                     children: [
-                      const SizedBox(height: 59),
+                      const Expanded(child: Empty()),
                       Text("Setting up Bison Relay",
                           style: TextStyle(
                               color: textColor,
@@ -126,20 +189,20 @@ class _NeedsFundsScreenState extends State<NeedsFundsScreen> {
                               color: secondaryTextColor,
                               fontSize: 21,
                               fontWeight: FontWeight.w300)),
-                      const SizedBox(height: 34),
+                      const SizedBox(height: 20),
                       Text('''
-The wallet requires on-chain DCR funds to be able to open Lightning Network (LN)
-channels and perform payments to the server and other users of the Bison Relay
-network.
+The wallet requires on-chain DCR funds to be able to open Lightning Network (LN) channels
+and perform payments to the server and other users of the Bison Relay network.
 
-Send DCR funds to the following address to receive funds in your wallet. Note that
-the wallet seed will be needed to recover these funds if the wallet data in this
-computer is corrupted or lost.
+Send DCR funds to the following address to receive funds in your wallet. Note that the
+wallet seed will be needed to recover these funds if the wallet data in this computer is
+corrupted or lost.
 ''',
                           style: TextStyle(
                               color: secondaryTextColor,
                               fontSize: 13,
                               fontWeight: FontWeight.w300)),
+                      buildFundsWidget(context),
                       const SizedBox(height: 13),
                       Container(
                           margin: const EdgeInsets.all(10),
@@ -196,11 +259,12 @@ computer is corrupted or lost.
                                     fontSize: 13,
                                     fontWeight: FontWeight.w300))
                           ])),
-                      const SizedBox(height: 34),
+                      const SizedBox(height: 20),
                       LoadingScreenButton(
                         onPressed: () => Navigator.of(context).pop(),
                         text: "Finish",
-                      )
+                      ),
+                      const Expanded(child: Empty()),
                     ],
                   )),
             ])));
