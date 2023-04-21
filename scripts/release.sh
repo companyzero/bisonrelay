903e7a20344c9a72433b83b1e8f9ca42064eb731
David Hill <dhill@mindcry.org>
2023-04-21 14:07:22 -0400

    scripts: brclient build

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Simple bash script to build basic ZKC tools for all the platforms
-# we support with the golang cross-compiler.
-#
-# Copyright (c) 2016 Company 0, LLC.
-# Use of this source code is governed by the ISC
-# license.
-
 # If no tag specified, use date + version otherwise use tag.
 if [[ $1x = x ]]; then
     DATE=`date +%Y%m%d`
@@ -16,38 +9,19 @@ else
     TAG=$1
 fi
 
-PACKAGE=zkc
-MAINDIR=$PACKAGE-$TAG
-mkdir -p $MAINDIR
-cd $MAINDIR
+PACKAGE=brclient
 
-SYS="windows-386 windows-amd64 linux-386 linux-amd64 linux-arm linux-arm64 darwin-amd64"
-
-# Use the first element of $GOPATH in the case where GOPATH is a list
-# (something that is totally allowed).
-GPATH=$(echo $GOPATH | cut -f1 -d:)
+SYS="windows-amd64 linux-amd64 linux-arm64 darwin-amd64 darwin-arm64"
 
 for i in $SYS; do
     OS=$(echo $i | cut -f1 -d-)
     ARCH=$(echo $i | cut -f2 -d-)
-    mkdir $PACKAGE-$i-$TAG
-    cd $PACKAGE-$i-$TAG
     echo "Building:" $OS $ARCH
-    env GOOS=$OS GOARCH=$ARCH go build github.com/companyzero/bisonrelay/zkclient
-    env GOOS=$OS GOARCH=$ARCH go build github.com/companyzero/bisonrelay/zkserver
-    env GOOS=$OS GOARCH=$ARCH go build github.com/companyzero/bisonrelay/tools/b64cert
-    env GOOS=$OS GOARCH=$ARCH go build github.com/companyzero/bisonrelay/tools/zkexport
-    env GOOS=$OS GOARCH=$ARCH go build github.com/companyzero/bisonrelay/tools/zkimport
-    env GOOS=$OS GOARCH=$ARCH go build github.com/companyzero/bisonrelay/tools/zkservertoken
-    cp $GPATH/src/github.com/companyzero/bisonrelay/zkclient/zkclient.conf .
-    cp $GPATH/src/github.com/companyzero/bisonrelay/zkserver/zkserver.conf .
-    cd ..
+    env CGO_ENABLED=0 GOOS=$OS GOARCH=$ARCH \
+	go build -o build/$PACKAGE-$OS-$ARCH-$TAG -v -trimpath -tags 'safe,netgo' -ldflags 'buildid=' 
     if [[ $OS = "windows" ]]; then
-	zip -r $PACKAGE-$i-$TAG.zip $PACKAGE-$i-$TAG
-    else
-	tar -cvzf $PACKAGE-$i-$TAG.tar.gz $PACKAGE-$i-$TAG
+	mv build/$PACKAGE-$i-$TAG build/$PACKAGE-$i-$TAG.exe
     fi
-    rm -r $PACKAGE-$i-$TAG
 done
 
-sha256sum * > manifest-$TAG.txt
+openssl sha256 -r build/* > build/manifest-$PACKAGE-$TAG.txt
