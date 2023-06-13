6b854f828e974a813c838d4ccee03f11a1a7166e
miki <mikitotefu@companyzero.com>
2023-06-13 13:12:22 +0000

    bruig: ios build changes

--- bruig/golibbuilder/ios.go
+++ bruig/golibbuilder/ios.go
@@ -4,4 +4,4 @@
 package golibbuilder
 
 //go:generate gomobile bind -target ios -o ../build/ios/Golib.xcframework ../golib
-//go:generate cp -r ../build/ios/Golib.xcframework ../flutterui/plugin/ios/Frameworks/Golib.xcframework
+//go:generate cp -r ../build/ios/Golib.xcframework ../flutterui/plugin/ios/Frameworks/
