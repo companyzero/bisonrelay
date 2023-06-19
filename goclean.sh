3210f1fe07e904241722dc0e5d8c9ca1f1b81358
miki <mikitotefu@companyzero.com>
2023-06-19 12:01:19 +0000

    github: move commands to workflow

--- goclean.sh
+++ goclean.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
-# The script does automatic checking on a Go package and its sub-packages, including:
-# 1. gofmt         (http://golang.org/cmd/gofmt/)
-# 2. go vet        (http://golang.org/cmd/vet)
-# 3. gosimple      (https://github.com/dominikh/go-simple)
-# 4. unconvert     (https://github.com/mdempsky/unconvert)
-# 5. ineffassign   (https://github.com/gordonklaus/ineffassign)
-# 6. unused        (https://github.com/dominikh/go-tools)
-# 7. test coverage (http://blog.golang.org/cover)
-#
+
 set -ex
 
 # run tests
-env GORACE="halt_on_error=1" go test ./...
-
-# golangci-lint (github.com/golangci/golangci-lint) is used to run each each
-# static checker.
+env go test ./...
 
 # check linters
 golangci-lint run
@@ -22,6 +11,3 @@ golangci-lint run
 # check client protobuf linters
 (cd clientrpc && protolint lint .)
 
-# To submit the test coverage result to coveralls.io,
-# use goveralls (https://github.com/mattn/goveralls)
-# goveralls -coverprofile=profile.cov -service=travis-ci
