4c5a946a2188eb95b54b072ab21e3ffd5c55abf7
Alex Yocom-Piatt <ay-p@decred.org>
2023-08-17 09:34:52 -0400

    bruig: fix runner and version script

--- scripts/bumpversion.sh
+++ scripts/bumpversion.sh
@@ -14,5 +14,5 @@ sed -Ei 's/Version = ".+"/Version = "'$VERSION'"/' brclient/internal/version/ver
 sed -Ei 's/Version = ".+"/Version = "'$VERSION'"/' brserver/internal/version/version.go
 sed -Ei 's/version: .+/version: '$VERSION'/' bruig/flutterui/bruig/pubspec.yaml
 sed -Ei 's/msix_version: .+/msix_version: '$VERSION'.0/' bruig/flutterui/bruig/pubspec.yaml
-sed -Ei 's/VERSION_AS_STRING ".+"/VERSION_AS_STRING "'$VERSION'"/' bruig/flutterui/bruig/windows/runner/Runner.rc
-sed -Ei 's/VERSION_AS_NUMBER [0-9,]+/VERSION_AS_NUMBER '$VERSION_NB_LIST'/' bruig/flutterui/bruig/windows/runner/Runner.rc
+sed -Ei 's/define VERSION_AS_STRING ".+"/define VERSION_AS_STRING "'$VERSION'"/' bruig/flutterui/bruig/windows/runner/Runner.rc
+sed -Ei 's/define VERSION_AS_NUMBER [0-9,]+/define VERSION_AS_NUMBER '$VERSION_NB_LIST'/' bruig/flutterui/bruig/windows/runner/Runner.rc
