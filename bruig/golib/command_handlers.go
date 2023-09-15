a5ed443511a9f4e30b9c81284eabd601329dec6c
Alex Yocom-Piatt <ay-p@decred.org>
2023-09-15 12:16:42 -0400

    pull expiration days from server for deffault

--- bruig/golib/command_handlers.go
+++ bruig/golib/command_handlers.go
@@ -65,6 +65,10 @@ type clientCtx struct {
 	// are about to be initiated.
 	downloadConfMtx   sync.Mutex
 	downloadConfChans map[zkidentity.ShortID]chan bool
+
+	// expirationDays are the expirtation days provided by the server when
+	// connected
+	expirationDays uint64
 }
 
 var (
@@ -306,6 +310,7 @@ func handleInitClient(handle uint32, args initClient) error {
 		}
 		st := serverSessState{State: state}
 		notify(NTServerSessChanged, st, nil)
+		cctx.expirationDays = expDays
 	}))
 
 	ntfns.Register(client.OnTipAttemptProgressNtfn(func(ru *client.RemoteUser, amtMAtoms int64, completed bool, attempt int, attemptErr error, willRetry bool) {
@@ -1786,9 +1791,10 @@ func handleClientCmd(cc *clientCtx, cmd *cmd) (interface{}, error) {
 		if age > 0 {
 			interval = time.Duration(age) * 24 * time.Hour
 		} else {
-			// Use default of 30 for now, need to pull expiration days from
-			// server
-			interval = time.Duration(30) * 24 * time.Hour
+			// Use server expiration days if none provided
+			cc.log.Debugf("Resetting all KX older than server"+
+				" expiration day setting: %v days", cc.expirationDays)
+			interval = time.Duration(cc.expirationDays) * 24 * time.Hour
 		}
 		res, err := c.ResetAllOldRatchets(interval, nil)
 		if err != nil {
