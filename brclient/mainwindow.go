8e0d75bf0daa9f24df323c9d8d97ad1d4066f97f
David Hill <dhill@mindcry.org>
2024-03-11 13:17:42 -0400

    brclient: fix ctrl up/down for mainwindow

--- brclient/mainwindow.go
+++ brclient/mainwindow.go
@@ -319,6 +319,22 @@ func (mws mainWindowState) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 				}
 			}
 
+		case msg.Type == tea.KeyCtrlDown:
+			if !mws.isPage {
+				if cw.changeSelected(1) {
+					mws.updateViewportContent()
+					mws.resetFormInput()
+				}
+			}
+
+		case msg.Type == tea.KeyCtrlUp:
+			if !mws.isPage {
+				if cw.changeSelected(-1) {
+					mws.updateViewportContent()
+					mws.resetFormInput()
+				}
+			}
+
 		case msg.Type == tea.KeyTab:
 			if mws.isPage {
 				if cw.changeSelected(1) {
