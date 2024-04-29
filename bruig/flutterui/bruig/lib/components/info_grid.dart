0f18dccfaca3064209335295899e85e6e832071f
miki <mikitotefu@companyzero.com>
2024-04-29 16:09:54 +0000

    bruig: fix ln channel list on mobile

--- bruig/flutterui/bruig/lib/components/info_grid.dart
+++ bruig/flutterui/bruig/lib/components/info_grid.dart
@@ -5,7 +5,15 @@ import 'package:tuple/tuple.dart';
 class SimpleInfoGrid extends StatelessWidget {
   final ScrollController? controller;
   final List<Tuple2<Widget, Widget>> items;
-  const SimpleInfoGrid(this.items, {Key? key, this.controller})
+  final int colValueFlex;
+  final double colLabelSize;
+  final double separatorWidth;
+  const SimpleInfoGrid(this.items,
+      {Key? key,
+      this.colLabelSize = 100,
+      this.colValueFlex = 4,
+      this.separatorWidth = 20,
+      this.controller})
       : super(key: key);
 
   @override
@@ -19,13 +27,17 @@ class SimpleInfoGrid extends StatelessWidget {
             margin: const EdgeInsets.only(bottom: 3),
             child: Row(
               children: [
+                /*
                 Flexible(
                   fit: FlexFit.tight,
+                  flex: colLabelFlex,
                   child: items[index].item1,
                 ),
-                const SizedBox(width: 20),
+                */
+                SizedBox(width: colLabelSize, child: items[index].item1),
+                SizedBox(width: separatorWidth),
                 Flexible(
-                  flex: 4,
+                  flex: colValueFlex,
                   child: items[index].item2,
                 ),
               ],
