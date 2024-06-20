061db816c29b0491bd04d390494896fbe6b62bfe
miki <mikitotefu@companyzero.com>
2024-06-20 14:47:39 +0000

    bruig: use named themes

--- bruig/flutterui/bruig/lib/theme_manager.dart
+++ bruig/flutterui/bruig/lib/theme_manager.dart
@@ -11,75 +11,98 @@ import './storage_manager.dart';
 //  secondary orange: ed6d47
 //  fontFamily: "SourceCodePro"),
 //  colorScheme: ColorScheme.fromSeed(seedColor: Color(0xff2970ff))),
+String _emojifont = Platform.isWindows ? "notoemoji_win" : "notoemoji_unix";
+
+class AppTheme {
+  final String key;
+  final String descr;
+  final ThemeData data;
+
+  AppTheme({required this.key, required this.descr, required this.data});
+}
+
+final appThemes = {
+  "dark": AppTheme(
+      key: "dark",
+      descr: "Dark Theme",
+      data: ThemeData(
+          fontFamily: "Inter",
+          fontFamilyFallback: [_emojifont],
+          //primarySwatch: Colors.blue,
+          primaryColor: Colors.black,
+          brightness: Brightness.dark,
+          backgroundColor: const Color(0xFF19172C),
+          highlightColor: const Color(0xFF252438),
+          dividerColor: const Color(0xFF8E8D98),
+          canvasColor: const Color(0xFF05031A),
+          cardColor: const Color(0xFF05031A),
+          errorColor: Colors.red,
+          focusColor: const Color(0xFFE4E3E6),
+          hoverColor: const Color(0xFF121026),
+          scaffoldBackgroundColor: const Color(0xFF19172C),
+          bottomAppBarColor: const Color(0xFF0175CE),
+          indicatorColor: const Color(0xFF5A5968),
+          shadowColor: const Color(0xFFE44B00),
+          dialogBackgroundColor: const Color(0xFF3A384B),
+          iconTheme: const IconThemeData(color: Color(0xFF8E8D98)),
+          primaryColorDark: Color(0xC0FCFCFC),
+          primaryColorLight: Color(0xFF0E0D0D),
+          textTheme: const TextTheme(
+              headline5: TextStyle(
+                color: Colors.white,
+                fontSize: 46,
+                fontWeight: FontWeight.w800,
+              ),
+              bodyText1: TextStyle(color: Colors.white),
+              bodyText2: TextStyle(
+                  color:
+                      Colors.black)))), // USE WITH RANDOM BACKGROUND FOR POSTS
+
+  "light": AppTheme(
+      key: "light",
+      descr: "Light Theme",
+      data: ThemeData(
+          fontFamily: "Inter",
+          fontFamilyFallback: [_emojifont],
+          //primarySwatch: Colors.blue,
+          primaryColor: Colors.white,
+          brightness: Brightness.light,
+          backgroundColor: const Color(0xFFE8E7F0),
+          highlightColor: const Color(0xFFDFDFE9),
+          dividerColor: const Color(0xFF68656E),
+          canvasColor: const Color(0xFFEDEBF8),
+          cardColor: const Color(0xFFEDEBF8),
+          errorColor: Colors.red,
+          focusColor: const Color(0xFF06030A),
+          hoverColor: const Color(0xFFDFDDEC),
+          scaffoldBackgroundColor: const Color(0xFFE8E7F3),
+          bottomAppBarColor: const Color(0xFF0175CE),
+          indicatorColor: const Color(0xFF5A5968),
+          shadowColor: const Color(0xFFE44B00),
+          dialogBackgroundColor: const Color(0xFFC6C5CF),
+          primaryColorDark: Color(0xC0FCFCFC),
+          primaryColorLight: Color(0xFF0E0D0D),
+          iconTheme:
+              const IconThemeData(color: Color.fromARGB(255, 101, 100, 110)),
+          textTheme: const TextTheme(
+              headline5: TextStyle(
+                color: Colors.black,
+                fontSize: 46,
+                fontWeight: FontWeight.w800,
+              ),
+              bodyText1: TextStyle(color: Colors.black),
+              bodyText2: TextStyle(color: Colors.white)))),
+
+  "system":
+      AppTheme(key: "system", descr: "Use System Default", data: ThemeData()),
+};
+
+const _defaultThemeName = "light"; // This MUST exist in the map above.
 
 class ThemeNotifier with ChangeNotifier {
   final double defaultFontSize = 1;
-  static String emojifont =
-      Platform.isWindows ? "notoemoji_win" : "notoemoji_unix";
-  final darkTheme = ThemeData(
-      fontFamily: "Inter",
-      fontFamilyFallback: [emojifont],
-      //primarySwatch: Colors.blue,
-      primaryColor: Colors.black,
-      brightness: Brightness.dark,
-      backgroundColor: const Color(0xFF19172C),
-      highlightColor: const Color(0xFF252438),
-      dividerColor: const Color(0xFF8E8D98),
-      canvasColor: const Color(0xFF05031A),
-      cardColor: const Color(0xFF05031A),
-      errorColor: Colors.red,
-      focusColor: const Color(0xFFE4E3E6),
-      hoverColor: const Color(0xFF121026),
-      scaffoldBackgroundColor: const Color(0xFF19172C),
-      bottomAppBarColor: const Color(0xFF0175CE),
-      indicatorColor: const Color(0xFF5A5968),
-      shadowColor: const Color(0xFFE44B00),
-      dialogBackgroundColor: const Color(0xFF3A384B),
-      iconTheme: const IconThemeData(color: Color(0xFF8E8D98)),
-      primaryColorDark: Color(0xC0FCFCFC),
-      primaryColorLight: Color(0xFF0E0D0D),
-      textTheme: const TextTheme(
-          headline5: TextStyle(
-            color: Colors.white,
-            fontSize: 46,
-            fontWeight: FontWeight.w800,
-          ),
-          bodyText1: TextStyle(color: Colors.white),
-          bodyText2: TextStyle(
-              color: Colors.black))); // USE WITH RANDOM BACKGROUND FOR POSTS
 
-  final lightTheme = ThemeData(
-      fontFamily: "Inter",
-      fontFamilyFallback: [emojifont],
-      //primarySwatch: Colors.blue,
-      primaryColor: Colors.white,
-      brightness: Brightness.light,
-      backgroundColor: const Color(0xFFE8E7F0),
-      highlightColor: const Color(0xFFDFDFE9),
-      dividerColor: const Color(0xFF68656E),
-      canvasColor: const Color(0xFFEDEBF8),
-      cardColor: const Color(0xFFEDEBF8),
-      errorColor: Colors.red,
-      focusColor: const Color(0xFF06030A),
-      hoverColor: const Color(0xFFDFDDEC),
-      scaffoldBackgroundColor: const Color(0xFFE8E7F3),
-      bottomAppBarColor: const Color(0xFF0175CE),
-      indicatorColor: const Color(0xFF5A5968),
-      shadowColor: const Color(0xFFE44B00),
-      dialogBackgroundColor: const Color(0xFFC6C5CF),
-      primaryColorDark: Color(0xC0FCFCFC),
-      primaryColorLight: Color(0xFF0E0D0D),
-      iconTheme: const IconThemeData(color: Color.fromARGB(255, 101, 100, 110)),
-      textTheme: const TextTheme(
-          headline5: TextStyle(
-            color: Colors.black,
-            fontSize: 46,
-            fontWeight: FontWeight.w800,
-          ),
-          bodyText1: TextStyle(color: Colors.black),
-          bodyText2: TextStyle(color: Colors.white)));
-
-  late ThemeData _themeData = lightTheme;
+  late ThemeData _themeData = appThemes[_defaultThemeName]!.data;
   ThemeData getTheme() => _themeData;
 
   late String _themeMode = "";
@@ -122,36 +145,7 @@ class ThemeNotifier with ChangeNotifier {
 
   ThemeNotifier() {
     StorageManager.readData('themeMode').then((value) {
-      var themeMode = value ?? 'light';
-      if (themeMode == 'light') {
-        _themeData = lightTheme;
-        _themeMode = 'light';
-      } else if (themeMode == 'dark') {
-        debugPrint('setting dark theme');
-        _themeData = darkTheme;
-        _themeMode = 'dark';
-      } else if (themeMode == 'system') {
-        _themeMode = 'system';
-        // only check system if on mobile
-        if (Platform.isIOS || Platform.isAndroid) {
-          debugPrint('setting system theme');
-          var brightness =
-              WidgetsBinding.instance.platformDispatcher.platformBrightness;
-          if (brightness == Brightness.light) {
-            _themeData = lightTheme;
-          } else if (brightness == Brightness.dark) {
-            _themeData = darkTheme;
-          } else {
-            _themeData = lightTheme;
-          }
-        } else {
-          _themeData = lightTheme;
-        }
-      } else {
-        _themeMode = 'light';
-        _themeData = lightTheme;
-      }
-      notifyListeners();
+      switchTheme(value);
     });
     StorageManager.readData('fontCoef').then((value) {
       _fontSize = double.parse(value ?? "1");
@@ -159,32 +153,27 @@ class ThemeNotifier with ChangeNotifier {
     });
   }
 
-  void setDarkMode() async {
-    _themeMode = 'dark';
-    _themeData = darkTheme;
-    StorageManager.saveData('themeMode', 'dark');
-    notifyListeners();
-  }
-
-  void setLightMode() async {
-    _themeMode = 'light';
-    _themeData = lightTheme;
-    StorageManager.saveData('themeMode', 'light');
-    notifyListeners();
-  }
-
-  void setSystemMode() async {
-    _themeMode = 'system';
-    var brightness =
-        WidgetsBinding.instance.platformDispatcher.platformBrightness;
-    if (brightness == Brightness.light) {
-      _themeData = lightTheme;
-    } else if (brightness == Brightness.dark) {
-      _themeData = darkTheme;
-    } else {
-      _themeData = lightTheme;
+  void switchTheme(String value) async {
+    // When using the special theme "system", determine the theme based on
+    // the platform brightness.
+    String themeName = value;
+    if (value == "system" && (Platform.isIOS || Platform.isAndroid)) {
+      var brightness =
+          WidgetsBinding.instance.platformDispatcher.platformBrightness;
+      if (brightness == Brightness.dark) {
+        themeName = "dark";
+      } else {
+        themeName = "light";
+      }
+    } else if (value == "system") {
+      themeName = _defaultThemeName;
     }
-    StorageManager.saveData('themeMode', 'system');
+
+    var td = appThemes[themeName]?.data ?? appThemes[_defaultThemeName]!.data;
+    _themeData = td;
+    _themeMode = value;
+    await StorageManager.saveData('themeMode', value);
+    _clearTxtStyleCache();
     notifyListeners();
   }
 
