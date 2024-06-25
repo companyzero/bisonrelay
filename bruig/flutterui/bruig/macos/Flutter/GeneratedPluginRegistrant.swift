22ea2dd7dd707500224726d145f05cd752410599
miki <mikitotefu@companyzero.com>
2024-06-25 12:49:42 +0000

    bruig: support scanning QR codes invites
    The library that does QR code scanning requires bumping the
    minSdkVersion for Android.

--- bruig/flutterui/bruig/macos/Flutter/GeneratedPluginRegistrant.swift
+++ bruig/flutterui/bruig/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,6 +8,7 @@ import Foundation
 import file_selector_macos
 import flutter_local_notifications
 import golib_plugin
+import mobile_scanner
 import package_info_plus
 import path_provider_foundation
 import screen_retriever
@@ -20,6 +21,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   GolibPlugin.register(with: registry.registrar(forPlugin: "GolibPlugin"))
+  MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
   FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   ScreenRetrieverPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverPlugin"))
