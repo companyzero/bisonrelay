5e1f385260a50aef651a0939c6c8cb84ad143c7e
miki <mikitotefu@companyzero.com>
2025-08-22 14:53:55 +0000

    bruig: use UserSearchPanel on suggest KX modal

--- bruig/flutterui/bruig/lib/components/users_dropdown.dart
+++ bruig/flutterui/bruig/lib/components/users_dropdown.dart
@@ -1,24 +1,21 @@
-import 'package:bruig/components/text.dart';
+import 'package:bruig/components/interactive_avatar.dart';
 import 'package:bruig/models/client.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:bruig/theme_manager.dart';
-import 'package:dropdown_button2/dropdown_button2.dart';
 
 typedef ChatModelCB = Function(ChatModel? c);
 
 class UsersDropdown extends StatefulWidget {
   final ChatModelCB? cb;
-  final bool allowEmpty;
   final List<String> excludeUIDs;
-  final List<String>? limitUIDs;
   final String hintText;
+  final ClientModel client;
+  final FocusNode? focusNode;
   const UsersDropdown(
       {this.cb,
+      required this.client,
       super.key,
-      this.allowEmpty = false,
+      this.focusNode,
       this.excludeUIDs = const [],
-      this.limitUIDs,
       this.hintText = "Select user..."});
 
   @override
@@ -26,95 +23,58 @@ class UsersDropdown extends StatefulWidget {
 }
 
 class _UsersDropdownState extends State<UsersDropdown> {
+  ClientModel get client => widget.client;
   ChatModel? selected;
   String? selectedValue;
-  final TextEditingController textEditingController = TextEditingController();
+
+  final TextEditingController txtCtrl = TextEditingController();
+
+  List<DropdownMenuEntry<ChatModel>> entries = [];
+
+  List<DropdownMenuEntry<ChatModel>> recalcList() {
+    var list = client.hiddenChats.sorted + client.activeChats.sorted;
+    list.removeWhere((c) {
+      if (c.isGC) return true;
+      if (widget.excludeUIDs.isNotEmpty) {
+        return widget.excludeUIDs.contains(c.id);
+      }
+      return false;
+    });
+
+    list.sort((a, b) => a.nick.toLowerCase().compareTo(b.nick.toLowerCase()));
+    return list
+        .map((c) => DropdownMenuEntry(
+            value: c, label: c.nick, leadingIcon: ChatAvatar(c)))
+        .toList();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    entries = recalcList();
+    print("calculated list");
+  }
+
+  @override
+  void didUpdateWidget(covariant UsersDropdown oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    entries = recalcList();
+  }
 
   @override
   void dispose() {
-    textEditingController.dispose();
     super.dispose();
   }
 
-  final List<String> items = [
-    'A_Item1',
-    'A_Item2',
-    'A_Item3',
-    'A_Item4',
-    'B_Item1',
-    'B_Item2',
-    'B_Item3',
-    'B_Item4',
-  ];
   @override
   Widget build(BuildContext context) {
-    return Consumer2<ClientModel, ThemeNotifier>(
-        builder: (context, client, theme, child) {
-      List<ChatModel?> list =
-          client.activeChats.sorted.cast<ChatModel?>().toList();
-      list.addAll(client.hiddenChats.sorted.cast<ChatModel?>().toList());
-      list.removeWhere((c) => c != null ? c.isGC : false);
-      if (widget.limitUIDs != null) {
-        list.removeWhere((c) => !(widget.limitUIDs!.contains(c?.id)));
-      }
-      list.sort(
-          (a, b) => a!.nick.toLowerCase().compareTo(b!.nick.toLowerCase()));
-      if (widget.allowEmpty) {
-        list.insert(0, null);
-      }
-      // Only use chats that aren't in the exclude UID list
-      if (widget.excludeUIDs.isNotEmpty && list.isNotEmpty) {
-        list = list.where((e) => !widget.excludeUIDs.contains(e!.id)).toList();
-      }
-      List<String> items = list.map((c) => c != null ? c.nick : "").toList();
-      return DropdownButtonHideUnderline(
-        child: DropdownButton2<String>(
-          autofocus: true,
-          isExpanded: true,
-          isDense: false,
-          hint: Txt(widget.hintText),
-          items: items
-              .map((c) => DropdownMenuItem(
-                  value: c,
-                  child: Container(
-                      margin: const EdgeInsets.all(0),
-                      width: double.infinity,
-                      alignment: Alignment.centerLeft,
-                      child: Text(c))))
-              .toList(),
-          selectedItemBuilder: (BuildContext context) => list
-              .map(
-                (ChatModel? c) => Center(
-                    child: Txt.S(
-                  c != null ? c.nick : "Share globally",
-                )),
-              )
-              .toList(),
-          value: selectedValue,
-          onChanged: (value) {
-            setState(() {
-              selected = list.firstWhere((c) => c?.nick == value);
-              widget.cb != null ? widget.cb!(selected) : null;
-              selectedValue = value;
-            });
-          },
-          buttonStyleData: ButtonStyleData(
-            padding: const EdgeInsets.only(left: 14, right: 14),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(14),
-              border: Border.all(
-                color: theme.colors.outline,
-              ),
-            ),
-            elevation: 2,
-          ),
-          iconStyleData: const IconStyleData(
-            icon: Icon(Icons.keyboard_arrow_down_outlined),
-            iconSize: 20,
-          ),
-          dropdownStyleData: const DropdownStyleData(maxHeight: 200),
-        ),
-      );
-    });
+    return DropdownMenu<ChatModel>(
+      dropdownMenuEntries: entries,
+      controller: txtCtrl,
+      focusNode: widget.focusNode,
+      onSelected: (value) {
+        if (widget.cb != null) widget.cb!(value);
+      },
+    );
   }
 }
