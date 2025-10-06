a96c92c4d5904ff8462217089defe89e3dd4de59
miki <mikitotefu@companyzero.com>
2025-10-06 14:11:09 +0000

    brseeder: test server promotion rules

--- brseeder/server/config.go
+++ brseeder/server/config.go
@@ -8,13 +8,16 @@ import (
 )
 
 type config struct {
-	waitForMaster   time.Duration
 	tokens          map[string]struct{}
 	listeners       []net.Listener
 	log             slog.Logger
 	appName         string
 	httpTimeout     time.Duration
 	shutdownTimeout time.Duration
+
+	// waitForMaster is how long to wait during startup for some brserver to
+	// declare itself master before promoting one.
+	waitForMaster time.Duration
 
 	// offlineLimit is how long to wait for dcrlnd/db to come back online
 	// before demoting the master.
@@ -42,5 +45,12 @@ func WithListeners(listeners []net.Listener) Option {
 func WithLogger(log slog.Logger) Option {
 	return func(c *config) {
 		c.log = log
+	}
+}
+
+func withPromotionTimeLimits(waitForMaster, offlineLimit time.Duration) Option {
+	return func(c *config) {
+		c.waitForMaster = waitForMaster
+		c.offlineLimit = offlineLimit
 	}
 }
