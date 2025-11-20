07fb2caf590d02c6eb02344fcb9ac059d80e350d
Alex Yocom-Piatt <ay-p@decred.org>
2025-11-20 16:40:04 -0500

    bruig: Expand sidebars based on current fontScale

--- bruig/flutterui/bruig/lib/components/feed_bar.dart
+++ bruig/flutterui/bruig/lib/components/feed_bar.dart
@@ -1,6 +1,8 @@
 import 'package:bruig/components/containers.dart';
 import 'package:bruig/components/text.dart';
 import 'package:flutter/material.dart';
+import 'package:bruig/theme_manager.dart';
+import 'package:provider/provider.dart';
 
 class FeedBar extends StatelessWidget {
   final int selectedIndex;
@@ -9,26 +11,27 @@ class FeedBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SecondarySideMenuList(
-      width: 130,
-      items: [
-        ListTile(
-            title: const Txt.S("Feed"),
-            selected: selectedIndex == 0,
-            onTap: () => tabChange(0, null)),
-        ListTile(
-            title: const Txt.S("Your Posts"),
-            selected: selectedIndex == 1,
-            onTap: () => tabChange(1, null)),
-        ListTile(
-            title: const Txt.S("Subscriptions"),
-            selected: selectedIndex == 2,
-            onTap: () => tabChange(2, null)),
-        ListTile(
-            title: const Txt.S("New Post"),
-            selected: selectedIndex == 3,
-            onTap: () => tabChange(3, null)),
-      ],
-    );
+    return Consumer<ThemeNotifier>(
+        builder: (context, theme, _) => SecondarySideMenuList(
+              width: 130 * (theme.fontScale > 0 ? theme.fontScale : 1),
+              items: [
+                ListTile(
+                    title: Txt.S("Feed"),
+                    selected: selectedIndex == 0,
+                    onTap: () => tabChange(0, null)),
+                ListTile(
+                    title: const Txt.S("Your Posts"),
+                    selected: selectedIndex == 1,
+                    onTap: () => tabChange(1, null)),
+                ListTile(
+                    title: const Txt.S("Subscriptions"),
+                    selected: selectedIndex == 2,
+                    onTap: () => tabChange(2, null)),
+                ListTile(
+                    title: const Txt.S("New Post"),
+                    selected: selectedIndex == 3,
+                    onTap: () => tabChange(3, null)),
+              ],
+            ));
   }
 }
